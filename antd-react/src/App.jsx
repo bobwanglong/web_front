b749f29a9fde108b8c9a3f80658570850421a70e
class-bob <wanglongtin@163.com>
2023-04-20 18:27:06 +0800

    feat:添加antd的使用

---
+++ antd-react/src/App.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import { ConfigProvider, DatePicker, message } from 'antd'
+import dayjs from 'dayjs'
+import 'dayjs/locale/zh-cn'
+import zhCN from 'antd/locale/zh_CN'
+import 'antd/dist/reset.css'
+dayjs.locale('zh-cn')
+function App() {
+  const [date, setDate] = useState(null)
+  const handleChange = (value) => {
+    message.info(
+      `您选择的日期是: ${value ? value.format('YYYY年MM月DD日') : '未选择'}`
+    )
+    setDate(value)
+  }
+  return (
+    <ConfigProvider locale={zhCN}>
+      <div style={{ width: 400, margin: '100px auto' }}>
+        <DatePicker onChange={handleChange} />
+        <div style={{ marginTop: 16 }}>
+          当前日期：{date ? date.format('YYYY年MM月DD日') : '未选择'}
+        </div>
+      </div>
+    </ConfigProvider>
+  )
+}
+
+export default App
