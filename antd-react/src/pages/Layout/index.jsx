b652e5ebcb5fdd8c90bfa5649b0353efcd6cbd4c
class-bob <wanglongtin@163.com>
2023-04-21 18:37:52 +0800

    feat:添加layout

--- antd-react/src/pages/Layout/index.jsx
+++ antd-react/src/pages/Layout/index.jsx
@@ -1,46 +1,103 @@
-import { useStore } from '../../store'
 import { observer } from 'mobx-react-lite'
-const Layout = () => {
-  // 获取数据
-  const { apiListStore } = useStore()
+import { Outlet, Link, useLocation, useNavigate } from 'react-router-dom'
+import {
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+  AreaChartOutlined,
+  FastForwardOutlined,
+} from '@ant-design/icons'
+import { Layout, Menu, theme } from 'antd'
+import React from 'react'
 
-  const clickHandler = async () => {
-    await apiListStore.getApiList()
+import './index.scss'
+
+const { Header, Content, Footer, Sider } = Layout
+
+const MyLayout = () => {
+  // const [openKeys, setOpenKeys] = useState([])
+  // const [selectedKey, setSelectedKey] = useState([])
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken()
+
+  const ApiListHandler = ({ key, domEvent }) => {
+    console.log('触发', domEvent)
+    console.log('key', key)
   }
   return (
-    <>
-      <div>layout</div>
-      <div>
-        <button onClick={clickHandler}>点击获取api接口数据</button>
-        {/* <div>{JSON.stringify(apiListStore.apiList)}</div> */}
-        <div>
-          <ul>
-            <span style={{ color: 'green' }}>path:</span>
-            {apiListStore.path.map((item) => (
-              <li key={item.backend}>{item.path}</li>
-            ))}
-          </ul>
+    <Layout>
+      <Sider
+        breakpoint="lg"
+        collapsedWidth="0"
+        onBreakpoint={(broken) => {
+          console.log(broken)
+        }}
+        onCollapse={(collapsed, type) => {
+          console.log(collapsed, type)
+        }}>
+        <div className="sideInner">
+          <div className="logo" />
+          <Menu
+            theme="dark"
+            mode="inline"
+            defaultSelectedKeys={['4']}
+            // items={[
+            //   UserOutlined,
+            //   VideoCameraOutlined,
+            //   UploadOutlined,
+            //   AreaChartOutlined,
+            //   myIcon,
+            // ].map((icon, index) => ({
+            //   key: String(index + 1),
+            //   icon: React.createElement(icon),
+            //   label: `nav ${index + 1}`,
+            // }))}
+            onClick={ApiListHandler}
+            items={[
+              {
+                icon: React.createElement(VideoCameraOutlined),
+                label: '接口列表',
+                key: '1',
+              },
+              {
+                icon: React.createElement(UserOutlined),
+                label: '456',
+                key: '2',
+              },
+            ]}
+          />
         </div>
-        <div>
-          <ul>
-            <span style={{ color: 'blue' }}>pathRegexp:</span>
-            {apiListStore.pathRegexp.map((item) => (
-              <li key={item.backend}>{item.pathRegexp}</li>
-            ))}
-          </ul>
-        </div>
-
-        <div>
-          <ul>
-            <span style={{ color: 'red' }}>pathPrefix:</span>
-            {apiListStore.pathPrefix.map((item) => (
-              <li key={item.backend}>{item.pathPrefix}</li>
-            ))}
-          </ul>
-        </div>
-      </div>
-    </>
+      </Sider>
+      <Layout>
+        <Header
+          style={{
+            padding: 0,
+            background: colorBgContainer,
+          }}
+        />
+        <Content
+          style={{
+            margin: '24px 16px 0',
+          }}>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 'calc(100vh - 100px - 80px)',
+              background: colorBgContainer,
+            }}>
+            content
+          </div>
+        </Content>
+        <Footer
+          style={{
+            textAlign: 'center',
+          }}>
+          Ant Design ©2023 Created by Ant UED
+        </Footer>
+      </Layout>
+    </Layout>
   )
 }
 
-export default observer(Layout)
+export default observer(MyLayout)
---
+++ antd-react/src/pages/apilist/index.jsx
@@ -0,0 +1,47 @@
+import { useStore } from '../../store'
+import { observer } from 'mobx-react-lite'
+import React from 'react'
+const Api = () => {
+  // 获取数据
+  const { apiListStore } = useStore()
+
+  const clickHandler = async () => {
+    await apiListStore.getApiList()
+  }
+  return (
+    <>
+      <div>layout</div>
+      <div>
+        <button onClick={clickHandler}>点击获取api接口数据</button>
+        {/* <div>{JSON.stringify(apiListStore.apiList)}</div> */}
+        <div>
+          <ul>
+            <span style={{ color: 'green' }}>path:</span>
+            {apiListStore.path.map((item) => (
+              <li key={item.backend}>{item.path}</li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <span style={{ color: 'blue' }}>pathRegexp:</span>
+            {apiListStore.pathRegexp.map((item) => (
+              <li key={item.backend}>{item.pathRegexp}</li>
+            ))}
+          </ul>
+        </div>
+
+        <div>
+          <ul>
+            <span style={{ color: 'red' }}>pathPrefix:</span>
+            {apiListStore.pathPrefix.map((item) => (
+              <li key={item.backend}>{item.pathPrefix}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default observer(Api)
