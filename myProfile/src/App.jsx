9dd70bd60bca7b1d976327b06a5ee3169040712c
class-bob <wanglongtin@163.com>
2023-04-24 15:12:11 +0800

    feat:myProfile project init

---
+++ myProfile/src/App.jsx
@@ -0,0 +1,16 @@
+import Contact from './components/Contact'
+import Hero from './components/Hero'
+import Who from './components/Who'
+import Works from './components/Works'
+function App() {
+  return (
+    <div>
+      <Hero />
+      <Who />
+      <Works />
+      <Contact />
+    </div>
+  )
+}
+
+export default App
