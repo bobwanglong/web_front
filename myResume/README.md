aaa7b2dce18d8b872434d527dc2710d6dae886b2
class-bob <wanglongtin@163.com>
2023-05-05 13:42:14 +0800

    feat:navbar 自适应menu功能

--- myResume/README.md
+++ myResume/README.md
@@ -65,3 +65,36 @@ vite 配置 tailwindcss https://tailwindcss.com/docs/guides/vite
 遇到的问题：tailwind 样式未生效,导致原因：main.jsx 中未引入 index.css
 
 4.2 navBar
+自适应菜单功能
+
+```jsx
+<div className="sm:hidden flex flex-1 justify-end items-center">
+  <img
+    src={toggle ? close : menu}
+    alt="menu"
+    className="w-[28px] h-[28px] object-contain"
+    onClick={() => setToggle(!toggle)}
+  />
+
+  <div
+    className={`${
+      !toggle ? 'hidden' : 'flex'
+    } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
+    <ul className="list-none flex justify-end items-start flex-1 flex-col gap-4">
+      {navLinks.map((nav) => (
+        <li
+          key={nav.id}
+          className={`font-poppins font-medium cursor-pointer text-[16px] ${
+            active === nav.title ? 'text-white' : 'text-secondary'
+          }`}
+          onClick={() => {
+            setToggle(!toggle)
+            setActive(nav.title)
+          }}>
+          <a href={`#${nav.id}`}>{nav.title}</a>
+        </li>
+      ))}
+    </ul>
+  </div>
+</div>
+```
--- myResume/src/components/Navbar.jsx
+++ myResume/src/components/Navbar.jsx
@@ -7,6 +7,7 @@ import { logo, menu, close } from '../assets'
 import { navLinks } from '../constants'
 const Navbar = () => {
   const [active, setActive] = useState('')
+  const [toggle, setToggle] = useState(false) // menu控制
   return (
     <nav
       className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20`}>
@@ -31,10 +32,39 @@ const Navbar = () => {
                 active === nav.title ? 'text-white' : 'text-secondary'
               } hover:text-white text-[18px] font-medium cursor-pointer`}
               onClick={() => setActive(nav.title)}>
-              <a href={`#${nav.id}`}>{nav.title}</a>
+              <a href={`#${nav.id}`}>{nav.title}</a>{' '}
+              {/**# 表示滚动到指定位置 */}
             </li>
           ))}
         </ul>
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img
+            src={toggle ? close : menu}
+            alt="menu"
+            onClick={() => setToggle(!toggle)}
+            className="w-[28px] h-[28px] object-contain"
+          />
+          <div
+            className={`${
+              !toggle ? 'hidden' : 'flex'
+            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
+            <ul className="list-none flex justify-end items-start flex-1 flex-col gap-4">
+              {navLinks.map((nav) => (
+                <li
+                  key={nav.id}
+                  className={`font-poppins font-medium cursor-pointer text-[16px] ${
+                    active === nav.title ? 'text-white' : 'text-secondary'
+                  }`}
+                  onClick={() => {
+                    setToggle(!toggle)
+                    setActive(nav.title)
+                  }}>
+                  <a href={`#${nav.id}`}>{nav.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </nav>
   )
