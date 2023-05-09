bfd35fdfc5a1256c7f9087c7d5f523290a97e8e0
class-bob <wanglongtin@163.com>
2023-05-09 14:49:18 +0800

    feat(ES6):手动实现promise demo2

--- es6/promise/customer/demo2.js
+++ es6/promise/customer/demo2.js
@@ -1,4 +1,4 @@
-
+// 缺陷：不能异步执行
 // 三个状态 常量
 const PENDING = "pending"
 const FULFILLED = "fulfilled"
@@ -40,7 +40,84 @@ class MyPromiseDemo2 {
   }
 
   then (onFulfilled, onRejected) {
-    onFulfilled = typeof onFulfilled === "function" ? onFulfilled : (value) = value
-    onRejected = typeof onRejected === "function" ? onRejected : (reason) = reason
+    onFulfilled = typeof onFulfilled === "function" ? onFulfilled : (value) => value
+    onRejected = typeof onRejected === "function" ? onRejected : (reason) => { throw (reason) }
+
+    // if (this.status === PENDING) {
+    //   this.resolveQueues.push(onFulfilled)
+    //   this.rejectQueues.push(onRejected)
+    // }
+
+    // if (this.status === FULFILLED) {
+    //   onFulfilled(this.value)
+    // }
+
+    // if (this.status === REJECTED) {
+    //   onRejected(this.reason)
+    // }
+    // return this
+    const promise2 = new MyPromiseDemo2((resolve, reject) => {
+      // resolve
+      if (this.status === FULFILLED) {
+        let x = onFulfilled(this.value)
+        resolve(x)
+      }
+
+      // reject
+      if (this.status === REJECTED) {
+        let x = onRejected(this.reason)
+        reject(x) && reject
+      }
+
+      // pending
+      if (this.status === PENDING) {
+        this.resolveQueues.push((value) => {
+          let x = onFulfilled(value)
+          resolve(x)
+        })
+
+        this.rejectQueues.push((reason) => {
+          let x = onRejected(reason)
+          reject(x) && reject
+        })
+      }
+    })
+    return promise2
   }
 }
+
+// let p1 = new MyPromiseDemo2((resolve, reject) => {
+//   // let p2 = new Promise((resolve, reject) => {
+
+//   setTimeout(() => {
+//     reject("failed")
+//   }, 500)
+//   setTimeout(() => {
+//     resolve('成功了')
+//   }, 3000)
+
+
+
+// })
+
+// p1.then((data) => {
+//   console.log(data)
+// }, (err) => {
+//   console.log(err)
+// })
+const p1 = new MyPromiseDemo2((resolved, rejected) => {
+  resolved('resolved')
+})
+
+p1.then((res) => {
+  console.log(res)
+  return 'then1'
+})
+  .then((res) => {
+    console.log(res)
+    return 'then2'
+  })
+  .then((res) => {
+    console.log(res)
+    return 'then3'
+  })
