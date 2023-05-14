7d6197d1ffaa22adc0bd30347801ea863a6fbd2a
class-bob <wanglongtin@163.com>
2023-05-14 21:54:54 +0800

    feat(tailwindcss):实现一个shimmer button

--- myTailwind/src/App.jsx
+++ myTailwind/src/App.jsx
@@ -1,5 +1,58 @@
 function App() {
-  return <div className="text-3xl fond-bold underline bg-yellow-50">my</div>
+  return (
+    <main className=" min-h-screen flex justify-center items-center w-full">
+      {/* // <div className=" grid justify-items-center">
+    //   <span className="text-3xl underline bg-yellow-50  font-bold">
+    //     myAlter
+    //   </span>
+    // </div> */}
+      <button
+        type="button"
+        className=" px-24 py-8 text-3xl font-bold
+         bg-[conic-gradient(from_var(--shimmer-angle),theme(colors.green.950)_0%,theme(colors.red.100)_10%,theme(colors.yellow.950)_20%,theme(colors.slate.950)_30%,theme(colors.blue.950)_20%)]
+         animate-[shimmer_2.5s_linear_infinite]
+         rounded-[24px]
+          relative
+         after:flex after:absolute after:bg-slate-950 
+         after:inset-[2px] after:rounded-[22px]  
+         after:content-[attr(aria-label)]
+         after:items-center after:justify-center
+      "
+        aria-label="渐变demo1">
+        渐变demo1
+      </button>
+      <button
+        type="button"
+        className=" px-24 py-8 text-3xl font-bold
+         bg-[conic-gradient(from_var(--shimmer-angle),theme(colors.slate.950)_0%,theme(colors.slate.500)_10%,theme(colors.slate.950)_20%)] 
+         animate-[shimmer_2.5s_linear_infinite] 
+         rounded-[24px] 
+         relative
+          after:flex after:absolute after:bg-slate-950 
+          after:inset-[2px] after:rounded-[22px]  
+          after:content-[attr(aria-label)]
+          after:items-center after:justify-center
+         "
+        aria-label="渐变demo2">
+        <span className=" opacity-0"> 渐变demo2</span>
+      </button>
+      {/* <button
+        type="button"
+        className="
+        px-24 py-8 text-3xl font-bold 
+        bg-[conic-gradient(from_var(--shimmer-angle),theme(colors.slate.950)_0%,theme(colors.slate.500)_10%,theme(colors.slate.950)_20%)] 
+        animate-[shimmer_2.5s_linear_infinite] rounded-[24px]
+        relative
+        
+        after:flex after:absolute after:bg-slate-950 after:inset-[2px] after:rounded-[22px] 
+        after:content-[attr(aria-label)]
+        after:items-center after:justify-center
+        "
+        aria-label="生成 AI 内容">
+        <span className="opacity-0">生成 AI 内容</span>
+      </button> */}
+    </main>
+  )
 }
 
 export default App
